b1830d74bad66a38fb840ccf146b7c3c993f5296
GioRubioHolberton <759@holbertonschool.com>
2020-06-04 01:38:31 +0000

    pep8

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ Log Parsing """
-""" script that reads stdin line by line and computes metrics """
-
 
 import sys
 
 count = 0
 code_status = {"200": 0, "301": 0, "400": 0, "401": 0,
-             "403": 0, "404": 0, "405": 0, "500": 0}
+               "403": 0, "404": 0, "405": 0, "500": 0}
 total_size = 0
 
 try:
