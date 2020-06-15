db15a4c5f2830d9278a57d3c6609ad9ffe90ff8a
GioRubioHolberton <759@holbertonschool.com>
2020-06-15 19:30:09 +0000

    betty

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -8,16 +8,16 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *mov1 = list, *mov2= list->next;
+	listint_t *mov1 = list, *mov2 = list->next;
 
 	if (list == NULL)
 		return (0);
-	
+
 	while (mov2 != NULL && mov2->next != NULL && mov1 != NULL)
 	{
 		if (mov1 == mov2)
 			return (1);
-		
+
 		mov1 = mov1->next;
 		mov2 = mov2->next->next;
 	}
