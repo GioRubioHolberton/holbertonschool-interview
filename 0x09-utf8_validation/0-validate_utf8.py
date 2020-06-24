fe2f7c62d9313de3d040b0d4b9757cd02638d9f7
GioRubioHolberton <759@holbertonschool.com>
2020-06-24 22:52:16 +0000

    use startswith

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" UTF-8 validation """
+
+def validUTF8(data):
+    """ set represents a valid UTF-8 encoding. """
+
+    num = 0
+
+    for bit in data:
+        env = bin(bit).replace('0b', '').rjust(8, '0')[-8:]
+        if num == 0:
+            if env.startswith('110'):
+                num = 1
+            elif env.startswith('1110'):
+                num = 2
+            elif env.startswith('11110'):
+                num = 3
+            elif env.startswith('10'):
+                return False
+        else:
+            if not env.startswith('10'):
+                return False
+            num -= 1
+    return True
