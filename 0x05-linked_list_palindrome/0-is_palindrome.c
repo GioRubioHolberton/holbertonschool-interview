4437fe11551f285b6ede358a2498fa5d7af681db
GioRubioHolberton <759@holbertonschool.com>
2020-06-25 15:07:29 +0000

    is palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - is or not linked list is palindorme
+ * @head: pointer to pointer
+ * Return: 0 if is palinfrome, 1 if not
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int one = 0, two = 0;
+	listint_t *move = *head;
+	int array[1000000];
+
+	if (*head == NULL)
+		return (1);
+	if (head == NULL)
+		return (0);
+
+	while (move != NULL)
+	{
+		array[one++] = move->n;
+		move = move->next;
+	}
+
+	while (one < two)
+	{
+		if (array[two] != array[one - 1 - two])
+			return (0);
+		two++;
+	}
+	return (1);
+}
