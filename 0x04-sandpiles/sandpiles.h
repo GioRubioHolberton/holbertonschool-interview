2515e7f4a93df1c0cc5535695b104f97777a8ecb
GioRubioHolberton <759@holbertonschool.com>
2020-06-26 01:26:01 +0000

    add sandpiles.h

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,12 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void revise(int grid1[3][3]);
+void print(int grid[3][3]);
+void distribution(int grid[3][3]);
+
+#endif
